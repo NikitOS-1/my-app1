f36faa0f3df661e5ed4354d779693ba82d534fcb
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-06-29 23:03:56 +0300

    add font in css

--- src/App.js
+++ src/App.js
@@ -9,54 +9,6 @@ import News from './components/MainRender/News/News';
 import Music from './components/MainRender/Music/Music';
 import Settings from './components/MainRender/Settings/Settings';
 
-let postData = [
-  { post: 'new post', likeCount: 0 },
-  { post: 'YOOO', likeCount: 13 },
-  { post: 'HELOOw ', likeCount: 345 },
-  { post: ' NSMA', likeCount: 34 },
-  { post: 'new kjdjd', likeCount: 74 },
-  { post: 'new POSTII', likeCount: 14 },
-  { post: ' Lorem', likeCount: 324 },
-  { post: 'new What', likeCount: 0 },
-  { post: 'new sds', likeCount: 39 }
-];
-
-let messageData = [
-  {
-      userName: 'Nikita',
-      userId: 1,
-      message: 'Hello im nikita'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }, {
-      userName: 'Sasha',
-      userId: 2,
-      message: 'Hello im Sasha'
-  }
-];
-
 const App = (props) => {
   return (
     <BrowserRouter>
@@ -65,8 +17,8 @@ const App = (props) => {
         <Navbar />
         <div className='content'>
           <Routes>
-            <Route path='/profile/*' element={<Profile postData={postData} />} />
-            <Route path='/messages/*' element={<Messages messageData={messageData}/>} />
+            <Route path='/profile/*' element={<Profile postData={props.state.posts} />} />
+            <Route path='/messages/*' element={<Messages messageData={props.state.messages} />} />
             <Route path='/news/*' element={<News />} />
             <Route path='/music/*' element={<Music />} />
             <Route path='/settings/*' element={<Settings />} />
---
+++ src/redux/state.js
@@ -0,0 +1,21 @@
+let state = {
+    posts: [
+        { post: 'new post', likeCount: 0 },
+        { post: 'new post', likeCount: 0 },
+        { post: 'new post', likeCount: 0 }
+    ],
+    messages: [
+        {
+            userName: 'Nikita',
+            userId: 1,
+            message: 'Hello im nikita'
+        },
+        {
+            userName: 'Nikita',
+            userId: 1,
+            message: 'Hello im nikita'
+        }
+    ]
+};
+
+export default state;

a3fd4611544089949762be3751cce888bebcd891
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-08-22 23:51:27 +0300

    dispatch

--- src/redux/state.js
+++ src/redux/state.js
@@ -35,20 +35,21 @@ let store = {
         this._state.messagesPage.messages.message = text;
         this._callSubscriber(this._state);
     },
-    addPost(text) {
-        let newPost = {
-            post: text,
-            likeCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText(text) {
-        this._state.profilePage.newPostText = text;
-        this._callSubscriber(this._state);
-    },
     subscribe(observer) {
         this._callSubscriber = observer;
+    },
+    dispatch(action) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                post: text,
+                likeCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = text;
+            this._callSubscriber(this._state);
+        }
     }
 };
 
